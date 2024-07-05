a48d843f29f09fc66ea501517f43e26867fe57dd
Zandolfus <girardiluke@gmail.com>
2024-07-05 02:34:36 -0400

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,30 @@
+export const ASPECTSOFPOWER = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+ASPECTSOFPOWER.abilities = {
+  vitality: 'ASPECTSOFPOWER.Ability.vitality.long',
+  endurance: 'ASPECTSOFPOWER.Ability.endurance.long',
+  strength: 'ASPECTSOFPOWER.Ability.strength.long',
+  dexterity: 'ASPECTSOFPOWER.Ability.dexterity.long',
+  toughness: 'ASPECTSOFPOWER.Ability.toughness.long',
+  intelligence: 'ASPECTSOFPOWER.Ability.intelligence.long',
+  willpower: 'ASPECTSOFPOWER.Ability.willpower.long',
+  wisdom: 'ASPECTSOFPOWER.Ability.wisdom.long',
+  perception: 'ASPECTSOFPOWER.Ability.perception.long',
+
+};
+
+ASPECTSOFPOWER.abilityAbbreviations = {
+  vitality: 'ASPECTSOFPOWER.Ability.vitality.abbr',
+  endurance: 'ASPECTSOFPOWER.Ability.endurance.abbr',
+  strength: 'ASPECTSOFPOWER.Ability.strength.abbr',
+  dexterity: 'ASPECTSOFPOWER.Ability.dexterity.abbr',
+  toughness: 'ASPECTSOFPOWER.Ability.toughness.abbr',
+  intelligence: 'ASPECTSOFPOWER.Ability.intelligence.abbr',
+  willpower: 'ASPECTSOFPOWER.Ability.willpower.abbr',
+  wisdom: 'ASPECTSOFPOWER.Ability.wisdom.abbr',
+  perception: 'ASPECTSOFPOWER.Ability.perception.abbr',
+};
