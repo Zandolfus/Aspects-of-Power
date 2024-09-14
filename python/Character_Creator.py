d9188127e622c45481a2027454f6eb453c69fd87
Zandolfus <girardiluke@gmail.com>
2024-09-13 21:58:59 -0400

    more classes

--- python/Character_Creator.py
+++ python/Character_Creator.py
@@ -529,10 +529,12 @@ class Character:
             'knife artist': {'dexterity': 5, 'perception': 4, 'vitality': 3, 'endurance': 2, 'free_points': 4},
             'bloodmage': {'intelligence': 5, 'wisdom': 4, 'vitality': 3, 'willpower': 2, 'free_points': 4},
             'aspiring blade of light': {'strength': 5, 'dexterity': 4, 'vitality': 3, 'endurance': 2, 'free_points': 4},
-            'spearman': {'strength': 5, 'dexterity': 4, 'vitality': 3, 'endurance': 2, 'free_points': 4},
+            'beginner assassin': {'dexterity': 5, 'strength': 4, 'perception': 3, 'endurance': 2, 'free_points': 4},
             'icecaller': {'intelligence': 5, 'willpower': 4, 'vitality': 3, 'perception': 2, 'free_points': 4},
-
-
+            'clergyman': {'wisdom': 5, 'willpower': 4, 'vitality': 3, 'endurance': 2, 'free_points': 4},
+            'swashbuckler': {'strength': 5, 'dexterity': 4, 'vitality': 3, 'endurance': 2, 'free_points': 4},
+            'witch of ages': {'willpower': 5, 'intelligence': 4, 'wisdom': 3, 'vitality': 2, 'free_points': 4},
+            'curse eater': {'willpower': 5, 'perception': 4, 'vitality': 3, 'dexterity': 2, 'free_points': 4},
         }
         
         # Apply stat gains based on current level and class
